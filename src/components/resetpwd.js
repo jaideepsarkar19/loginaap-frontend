e8c1ca3f50c8a5ef7a631b220e379ffd3efc8d0d
jaideepsarkar19 <jaideep.sarkar19@rediffmail.com>
2023-05-11 22:01:59 +0530

    1st

---
+++ src/components/resetpwd.js
@@ -0,0 +1,49 @@
+import React,{useState} from "react";
+
+export default function Reset() {
+ const [email,setEmail]= useState("");
+function handleSubmit(e){
+    e.preventDefault();
+    // const {email}= req.body
+    console.log(email);
+    fetch("http://localhost:5000/forgot-password",{ 
+        method: "POST",
+        crossDomain: true,
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            "Access-Control-Allow-Origin": "*",
+                    }, 
+                    body: JSON.stringify({
+                        email,
+                              }),
+   } )
+   .then((res) => res.json())
+   .then((data) => {
+     console.log(data, "userRegister");
+    });
+}
+
+    return(
+    <form onSubmit={handleSubmit} >  
+    <h2> Forgot Password</h2>
+    <div className="mb-3"> 
+    <label> Email address</label>
+    <input
+    type = "email"
+    className = "form-control"
+    placeholder= "Enter email"
+    onChange={(e)=>setEmail({email:e.target.value})}
+    />
+    </div>
+     <div className= "d-grid">
+        <button type="submit" className ="btn btn-primary">
+           Submit 
+        </button>
+     </div>
+     <p className="forgot-password text-right">
+       <a href="/sign-up"> Sign Up</a> 
+     </p>
+    </form>
+    );
+  }
